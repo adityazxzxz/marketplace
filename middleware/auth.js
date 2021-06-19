d3a9bd730550f5b14ed7020b7e8116522328ff65
Aditya <adityapratamax11@gmail.com>
2021-06-19 19:43:17 +0700

    update

---
+++ middleware/auth.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+const users = require('../models/users')
+
+module.exports = (req,res,next) => {
+    try {
+        let token = req.get('Authorization')
+        if(token == null){
+            return res.status(401).json({
+                error:true,
+                message:'Unauthorized'
+            })
+
+        }
+        token = token.replace(/^Bearer\s/,"");
+
+        jwt.verify(token, 'ass123Hole',async (err, decoded) =>{
+            if(err){
+                return res.status(401).json({
+                    error:true,
+                    message:'unauthorized'
+                })
+            }
+
+            let tmp = await req.redis.get(decoded.session)
+            if(tmp){
+                req.decoded = decoded
+                return next()
+            }else{
+                console.log('redis not found')
+            }
+        })
+    } catch (error) {
+        
+    }
+}
